2cf99590dde4f7c418a5904a160c484ed60d3b53
Moseley <amoseley@unityprep.org>
2019-05-30 20:49:34 +0000

    saving this

--- js/script.js
+++ js/script.js
@@ -9,9 +9,10 @@ $('h1').click(function(){
 
 $('#searchButton').click(function() {
     var searchTerm = $("#searchTerm").val();
+    console.log('this is my search term', searchTerm)
     // replace spaces with encoded space
     var formattedSearchTerm = searchTerm.replace(' ', '%20')
-    var url = "https://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=3c29e935498d124ef58372d0141ec744";
+    var url = `https://api.openweathermap.org/data/2.5/weather?q=${searchTerm}&APPID=3c29e935498d124ef58372d0141ec744`;
     $.ajax({
         url: url,
         method: "GET",
